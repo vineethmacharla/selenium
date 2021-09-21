cfa5464cb7911a0f74d53314b6db1865e7adc1ed
vineethmacharla <88477289+vineethmacharla@users.noreply.github.com>
2021-09-21 08:22:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ time.sleep(3)
 driver.find_element_by_xpath("//*[@id='series-matches']/div[3]/div/a[2]").click()
 time.sleep(2)
 driver.find_element_by_xpath("//*[@id='matchCenter']/div[3]/div[2]/div[2]")
-time.sleep(2)
-x=driver.find_element_by_xpath("//*[@id='matchCenter']/div[3]/div[2]/div[2]").text
+time.sleep(3)
+x = driver.find_element_by_xpath("//*[@id='matchCenter']/div[3]/div[2]/div[2]").text
 print(x)
 time.sleep(5)
 driver.find_element_by_xpath("//*[@id='matchCenter']/div[2]/nav/a[2]").click()
 time.sleep(2)
+#y = driver.find_element_by_xpath("//*[@id='innings_1']/div[1]/div[2]").text
+#y = y.split('\n')
+#print(y)
 players = driver.find_elements_by_xpath("//*[@class='cb-col cb-col-100 cb-scrd-itms']")
-players = players[:10]
-y = []
+players = players[:37]
 for i in players:
-    # print(i.text)
-    y.append(i.text)
-    # print('----------------------------')
-for i in y:
-    i = i.split('\n')
-    if i[0].find('Thakur') >= 0:
-        print('{} Score is {}'.format(i[0], i[2]))
+    #print(i.text)
+    player_details=i.text
+    player_details =  player_details.split('\n')
+    if player_details[0].find('Pant (wk)') >= 0:
+        print(player_details)
+
